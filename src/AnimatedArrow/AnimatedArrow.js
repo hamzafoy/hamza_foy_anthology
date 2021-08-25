779efa931ceac33f112f02b6169abe01585bc228
Ross Foy <hamzaoflouisville@gmail.com>
2021-08-24 20:10:41 -0400

    replace a react-spring animation with a native CSS3 svg animation on landing page to indicate users to scroll down.

--- src/AnimatedArrow/AnimatedArrow.js
+++ src/AnimatedArrow/AnimatedArrow.js
@@ -1,22 +1,14 @@
-import React, { useState } from 'react';
-import { useSpring, animated, config } from 'react-spring';
+import React from 'react';
+import './AnimatedArrow.css';
 
 
 
-function AnimatedArrow() {
-    const [alternate, set] = useState(false);
-    const props = useSpring({
-        from: { opacity: 1 },
-        to: { opacity: 0 },
-        reset: true,
-        reverse: alternate,
-        delay: 400,
-        config: config.molasses,
-        onRest: () => set(!alternate)
-    })
+function AnimatedArrow() {  
 
     return (
-        <animated.svg
+        <>
+    
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             width="100"
             height="100"
@@ -25,21 +17,22 @@ function AnimatedArrow() {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="1.5"
-            className="feather feather-arrow-down-circle"
             viewBox="0 0 24 24"
-            style={props}
-    >
-        <mask id="mask">
-            <rect x="0" y="0" width="100%" height="100%" fill="white" />
-        </mask>
-        <circle cx="12" cy="12" r="10" mask="url(#mask)"/>
+          >
 
-        <g stroke="currentColor">
-        <path d="M8 12L12 16 16 12"></path>
-        <path d="M12 8L12 16"></path>
-        </g>
+            <g id="light" opacity="0.6">
+              <path id="first-arrow" d="M7 13L12 18 17 13"></path>
+            </g>
+            <g id="dark" opacity="0.6">
+              <path id="second-arrow" d="M7 9L12 14 17 9"></path>
+            </g>
+            <g id="light" opacity="0.6">
+              <path id="third-arrow" d="M7 5L12 10 17 5"></path>
+            </g>
 
-    </animated.svg>
+          </svg>
+    
+        </>
     )
 
 }
