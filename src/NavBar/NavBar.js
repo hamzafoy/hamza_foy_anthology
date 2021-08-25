57138cbacdb0470c61a47cfe6483b1a5a8432ae1
Ross Foy <hamzaoflouisville@gmail.com>
2021-08-25 17:59:37 -0400

    finish much of the structure of the navbar, inputting links and icons, will begin working on styling of the navbar.

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,25 +1,49 @@
 import React from 'react';
 import './NavBar.css';
+import logo from './resources/hamza_Foy_Logo.png';
+import github from './resources/github.svg';
+import linkedin from './resources/linkedin.svg';
+import resume from './resources/resume-2.svg';
+import resumePDF from './resources/docs/Hamza_Foy_Resume.pdf';
 
 
 
 class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            titles: ['Freelance Web Developer', 'Respected Community Member', 'Junior Full-Stack Engineer', 'Small Business Aficionado', 'Marketing Material Designer']
+        }
+    }
 
     render() {
+        const dayAndNight = new Date().getHours();
+        let randomNumber = Math.floor((Math.random() * 5));
 
         return(
             <div className="nav-bar-container">
 
-                <section className="nav-bar__logo">
-                    <h1>Testing</h1>
-                </section>
-
-                <section className="nav-bar__byline">
-                    <h1>Another Test</h1>
+                <section className="nav-bar__logo-and-byline">
+                    <img className="logo-png" src={logo} alt="Hamza Foy Web Design"/>
+                    <h1 className="byline">
+                        Hamza Foy &#9876; {this.state.titles[randomNumber]} by {dayAndNight >= 8 && dayAndNight <= 20 ? 'day' : 'night'}!
+                    </h1>
                 </section>
 
                 <section className="nav-bar__icons">
-                    <h1>A Third Test!</h1>
+
+                    <a href="https://github.com/hamzafoy" target="_blank" rel="noreferrer">
+                        <img className="icon" src={github} alt="Hamza's Github"/>
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/ross-hamza-foy/" target="_blank" rel="noreferrer">
+                        <img className="icon" src={linkedin} alt="Hamza's LinkedIn"/>
+                    </a>
+
+                    <a href={resumePDF} target="_blank" rel="noreferrer" download>
+                        <img className="icon" src={resume} alt="Hamza's Resume"/>
+                    </a>
+
                 </section>
             </div>
         )
