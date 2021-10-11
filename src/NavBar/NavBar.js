69509a22dc3934690c56f206950d071c88301e12
Ross Foy <hamzaoflouisville@gmail.com>
2021-10-10 23:05:54 -0400

    animated email icon and whatsapp icon.

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -5,10 +5,8 @@
 import React from 'react';
 import './NavBar.css';
 import github from './resources/svg/github.svg';
-import linkedin from './resources/svg/linkedin.svg';
 import email from './resources/svg/mail.svg';
 import resume from './resources/svg/resume.svg';
-import whatsapp from './resources/svg/whatsapp.svg';
 import resumePDF from './resources/doc/Hamza_Foy_Resume.pdf';
 
 
@@ -67,18 +65,23 @@ class NavBar extends React.Component {
                     </a>
 
                     <a 
-                    className="icon__linkedin"
                     href="https://www.linkedin.com/in/ross-hamza-foy/" 
                     target="_blank" 
                     rel="noreferrer"
                     >
 
-                        <img 
-                        className="icon" 
-                        src={linkedin} 
-                        alt="Hamza's LinkedIn"
-                        title="Connect with me!"
-                        />
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon__linkedin"
+                        viewBox="0 0 24 24"
+                        >
+
+                            <path d="M4.98 3.5C4.98 4.881 3.87 6 2.5 6S.02 4.881.02 
+                            3.5C.02 2.12 1.13 1 2.5 1s2.48 1.12 2.48 2.5zM5 
+                            8H0v16h5V8zm7.982 0H8.014v16h4.969v-8.399c0-4.67 6.029-5.052
+                             6.029 0V24H24V13.869c0-7.88-8.922-7.593-11.018-3.714V8z"></path>
+                            
+                        </svg>
 
                     </a>
 
@@ -104,12 +107,25 @@ class NavBar extends React.Component {
                     rel="noreferrer"
                     >
 
-                        <img 
-                        className="icon" 
-                        src={whatsapp} 
-                        alt="Hamza's Whatsapp"
-                        title="Message me!"
-                        />
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon__whatsapp"
+                        viewBox="0 0 24 24"
+                        >
+
+                            <path d="M.057 24l1.687-6.163a11.867 11.867 0 01-1.587-5.946C.16 
+                            5.335 5.495 0 12.05 0a11.817 11.817 0 018.413 3.488 11.824 11.824 
+                            0 013.48 8.414c-.003 6.557-5.338 11.892-11.893 11.892a11.9 11.9 0
+                             01-5.688-1.448L.057 24zm6.597-3.807c1.676.995 3.276 1.591 5.392 
+                             1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 
+                             0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 
+                             3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941
+                              1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 
+                              0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 
+                              2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 
+                              1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"></path>
+                            
+                        </svg>
 
                     </a>
 
@@ -119,12 +135,23 @@ class NavBar extends React.Component {
                     rel="noreferrer"
                     >
 
-                        <img 
-                        className="icon" 
-                        src={email} 
-                        alt="Hamza's Email"
-                        title="Email me!"
-                        />
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        stroke="black"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="icon icon__email"
+                        viewBox="0 0 24 24"
+                        >
+
+                            <path className="svg-envelope" d="M4 4h16c1.1 0 2 .9 2 2v12c0 
+                            1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                            <path className="svg-envelope-flap" d="M22 6L12 13 2 6"></path>
+                            <text className="svg-envelope-text" x="9" y="14.5">T</text>
+
+                        </svg>
 
                     </a>
 
