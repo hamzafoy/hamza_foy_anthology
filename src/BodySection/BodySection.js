2f36fc3864b0d9f4bbb7f92d966282ebff51713b
Ross Foy <hamzaoflouisville@gmail.com>
2021-10-15 00:55:40 -0400

    add additional completed project cards to middle container.

--- src/BodySection/BodySection.js
+++ src/BodySection/BodySection.js
@@ -35,7 +35,24 @@ class BodySection extends React.Component {
 
                         <p className="item__text">
                             I developed this website for food truck owner Abdullah Mahmoud. I utilize 
-                            Surge.sh SSG to push updates requested by client in a swift fashion.
+                            Surge.sh SSG to push updates requested by client in a swift fashion.<br/><br/>
+
+                            Tech Used: HTML5, CSS3, JavaScript
+                        </p>
+
+                    </aside>
+
+                    <aside className="project-list__item">
+
+                        <h1 className="item__label">
+                            Ihya502 Podcast
+                        </h1>
+
+                        <p className="item__text">
+                            I developed this full-stack web app for a local podcast I produce. I have the 
+                            front end hosted on Vercel &amp; the back end hosted on Heroku.<br/><br/>
+
+                            Tech Used: React.js, MongoDB, Mongoose, CSS3, Express.js, Node.js
                         </p>
 
                     </aside>
